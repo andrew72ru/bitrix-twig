0b2202d6e66548db1235b7410d734c82dfe8c99b
Андрей Ждановских <andrew72ru@gmail.com>
2018-06-06 16:53:12 +0500

    Add twig/extensions package

--- src/TemplateEngine.php
+++ src/TemplateEngine.php
@@ -155,6 +155,7 @@ class TemplateEngine
 
         $this->engine->addExtension(new PhpGlobals());
         $this->engine->addExtension(new BitrixExtension());
+        $this->engine->addExtension(new \Twig_Extensions_Extension_Intl());
     }
 
     /**
